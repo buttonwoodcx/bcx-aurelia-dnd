cabf25588c952f6447ec251daae3453e75b67cd4
Chunpeng Huo <huocp@me.com>
2017-06-21 16:57:17 +1000

    remove unneeded injection. static injection on parent class still works.

--- src/resources/attributes/if-not.js
+++ src/resources/attributes/if-not.js
@@ -1,14 +1,8 @@
-import {inject} from 'aurelia-framework';
-import {BoundViewFactory, ViewSlot, templateController} from 'aurelia-templating';
+import {templateController} from 'aurelia-templating';
 import {If} from 'aurelia-templating-resources';
 
 @templateController
-@inject(BoundViewFactory, ViewSlot)
 export class IfNotCustomAttribute extends If {
-  constructor(viewFactory, viewSlot) {
-    super(viewFactory, viewSlot);
-  }
-
   valueChanged(newValue) {
     super.valueChanged(!newValue);
   }
